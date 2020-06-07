66de6873b8428f4b59d741832b9db4c3d021cc59
Carlos Miceli <carlos@Carloss-MacBook-Pro-2.local>
2020-06-07 19:57:20 -0300

    homework jsII

---
+++ henry-Prep/Lesson05-JS-II/homework/fran test.js
@@ -0,0 +1,9 @@
+maximo = primer elemento de la lista
+minimo = primer elemento de la lista
+Para cada elemento i de la lista:
+SI (i > maximo):
+maximo = i
+SI NO, SI (i < minimo):
+minimo = i
+
+retorno maximo, minimo
